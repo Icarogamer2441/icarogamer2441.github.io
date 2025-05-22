404b22298f3064aecb33b38c7e4d54ca6b707c49
Icarogamer2441 <148513434+Icarogamer2441@users.noreply.github.com>
2025-05-22 12:17:54 -0300

    Create StartupMod.js

---
+++ jsvival/mods/StartupMod.js
@@ -0,0 +1,12 @@
+JSV.api.modname = "StartupMod";
+JSV.api.modversion = "1.0.0";
+JSV.api.moddescription = "A test mod to demonstrate the modding API.";
+JSV.api.modauthor = "JSVival developer <3";
+
+function startup() {
+    // This function runs when the mod is loaded
+    JSV.api.send_message("StartupMod has been loaded!");
+    JSV.api.send_message("if you're running this mod on github,");
+    JSV.api.send_message("you can't add mods, to add mods,");
+    JSV.api.send_message("locally, clone https://github.com/Icarogamer2441/JSVival.git");
+}
